d9dee95a2dcf39600b2fe2709fab87af3c89ec37
SeaMan75 <bgnick@mail.ru>
2024-12-31 19:21:08 +0300

    Add files via upload

---
+++ StylesN_H1_H2.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Styles
+{
+    partial class StylesN_H1_H2 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public StylesN_H1_H2()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.tab2 = this.Factory.CreateRibbonTab();
+            this.groupStyles = this.Factory.CreateRibbonGroup();
+            this.buttonStyles = this.Factory.CreateRibbonButton();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.button3 = this.Factory.CreateRibbonButton();
+            this.button4 = this.Factory.CreateRibbonButton();
+            this.button5 = this.Factory.CreateRibbonButton();
+            this.button6 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.tab2.SuspendLayout();
+            this.groupStyles.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // tab2
+            // 
+            this.tab2.Groups.Add(this.groupStyles);
+            this.tab2.Label = "Разное";
+            this.tab2.Name = "tab2";
+            // 
+            // groupStyles
+            // 
+            this.groupStyles.Items.Add(this.buttonStyles);
+            this.groupStyles.Items.Add(this.button1);
+            this.groupStyles.Items.Add(this.button2);
+            this.groupStyles.Items.Add(this.button3);
+            this.groupStyles.Items.Add(this.button4);
+            this.groupStyles.Items.Add(this.button5);
+            this.groupStyles.Items.Add(this.button6);
+            this.groupStyles.Label = "Стили";
+            this.groupStyles.Name = "groupStyles";
+            // 
+            // buttonStyles
+            // 
+            this.buttonStyles.Label = "Настройка стилей";
+            this.buttonStyles.Name = "buttonStyles";
+            this.buttonStyles.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.buttonStyles_Click);
+            // 
+            // button1
+            // 
+            this.button1.Label = "Отступы у выделенного";
+            this.button1.Name = "button1";
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Label = "Поля: 2/2/3/1.5";
+            this.button2.Name = "button2";
+            this.button2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Label = "Поля 2/2/3/1";
+            this.button3.Name = "button3";
+            this.button3.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Label = "Стиль Картинка";
+            this.button4.Name = "button4";
+            this.button4.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Label = "Подпись картинки";
+            this.button5.Name = "button5";
+            this.button5.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Label = "Подпись таблицы";
+            this.button6.Name = "button6";
+            this.button6.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button6_Click);
+            // 
+            // StylesN_H1_H2
+            // 
+            this.Name = "StylesN_H1_H2";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.tab1);
+            this.Tabs.Add(this.tab2);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.StylesN_H1_H2_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.tab2.ResumeLayout(false);
+            this.tab2.PerformLayout();
+            this.groupStyles.ResumeLayout(false);
+            this.groupStyles.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        private Microsoft.Office.Tools.Ribbon.RibbonTab tab2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup groupStyles;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton buttonStyles;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button5;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button6;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal StylesN_H1_H2 StylesN_H1_H2
+        {
+            get { return this.GetRibbon<StylesN_H1_H2>(); }
+        }
+    }
+}
---
+++ StylesN_H1_H2.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using Word = Microsoft.Office.Interop.Word;
+using Microsoft.Office.Tools.Ribbon;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Word;
+
+namespace Styles
+{
+    public partial class StylesN_H1_H2
+    {
+        private void StylesN_H1_H2_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void buttonStyles_Click(object sender, RibbonControlEventArgs e)
+        {
+            Document doc = null;
+            Word.Style normalStyle = null;
+            Style heading1Style = null;
+            Word.Style heading2Style = null;
+
+            try
+            {
+                doc = Globals.ThisAddIn.Application.ActiveDocument;
+
+                if (doc == null)
+                {
+                    System.Windows.Forms.MessageBox.Show("Нет активного документа.");
+                    return;
+                }
+
+                // Проверка существования стиля "Normal"
+
+                normalStyle = doc.Styles[Word.WdBuiltinStyle.wdStyleNormal];
+                if (normalStyle != null)
+                {
+                    normalStyle.Font = new Word.Font
+                    {
+                        Name = "Times New Roman Cyr",
+                        Size = 14,
+                        Bold = 0,
+                        Color = Word.WdColor.wdColorAutomatic
+                    };
+                    normalStyle.ParagraphFormat = new Word.ParagraphFormat
+                    {
+                        FirstLineIndent = Globals.ThisAddIn.Application.CentimetersToPoints(1.25f),
+                        LineSpacingRule = Word.WdLineSpacing.wdLineSpace1pt5,
+                        SpaceBefore = 0,
+                        SpaceAfter = 0,
+                        Alignment = Word.WdParagraphAlignment.wdAlignParagraphJustify
+                    };
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Стиль 'Обычный' не найден.");
+                }
+
+                // Проверка существования стиля "Heading 1"
+                heading1Style = doc.Styles[Word.WdBuiltinStyle.wdStyleHeading1];
+                if (heading1Style != null)
+                {
+                    heading1Style.Font = new Word.Font
+                    {
+                        Name = "Times New Roman Cyr",
+                        Color = Word.WdColor.wdColorAutomatic,
+                        Size = 14,
+                        Bold = 1
+                    };
+
+                    heading1Style.ParagraphFormat = new Word.ParagraphFormat
+                    {
+                        SpaceBefore = 0,
+                        SpaceAfter = 28,
+                        LineSpacingRule = Word.WdLineSpacing.wdLineSpace1pt5,
+                        Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter,
+                        FirstLineIndent = 0,
+                        PageBreakBefore = -1
+                    };
+                    heading1Style.set_BaseStyle("");
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Стиль 'Заголовок 1' не найден.");
+                }
+
+                heading2Style = doc.Styles[Word.WdBuiltinStyle.wdStyleHeading2];
+                if (heading2Style != null)
+                {
+                    heading2Style.Font = new Word.Font
+                    {
+                        Name = "Times New Roman Cyr",
+                        Size = 14,
+                        Bold = 1,
+                        Color = Word.WdColor.wdColorAutomatic
+                    };
+                    heading2Style.ParagraphFormat = new Word.ParagraphFormat
+                    {
+                        SpaceBefore = 0,
+                        SpaceAfter = 28,
+                        LineSpacingRule = Word.WdLineSpacing.wdLineSpace1pt5,
+                        Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter,
+                        FirstLineIndent = 0,
+                        PageBreakBefore = 0
+                    };
+                    heading2Style.set_BaseStyle("");
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Стиль 'Заголовок 2' не найден.");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Произошла ошибка: " + ex.Message);
+            }
+            finally
+            {
+                if (normalStyle != null) Marshal.ReleaseComObject(normalStyle);
+                if (heading1Style != null) Marshal.ReleaseComObject(heading1Style);
+                if (heading2Style != null) Marshal.ReleaseComObject(heading2Style);
+                if (doc != null) Marshal.ReleaseComObject(doc);
+            }
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Document doc = null;
+            Style normalStyle = null;
+            Style heading1Style = null;
+            Style heading2Style = null;
+                    
+            try
+            {
+                doc = Globals.ThisAddIn.Application.ActiveDocument;
+
+                if (doc == null)
+                {
+                    System.Windows.Forms.MessageBox.Show("Нет активного документа.");
+                    return;
+                }
+                // Установка отступов и первой строки для всего выделенного текста
+                Selection selection = Globals.ThisAddIn.Application.Selection;
+                selection.ParagraphFormat = new Word.ParagraphFormat
+                {
+                    LeftIndent = 0,
+                    RightIndent = 0,
+                    FirstLineIndent = Globals.ThisAddIn.Application.CentimetersToPoints((float)1.25)
+                };
+            }
+
+
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Произошла ошибка: " + ex.Message);
+            }
+            finally
+            {
+                if (normalStyle != null) Marshal.ReleaseComObject(normalStyle);
+                if (heading1Style != null) Marshal.ReleaseComObject(heading1Style);
+                if (heading2Style != null) Marshal.ReleaseComObject(heading2Style);
+                if (doc != null) Marshal.ReleaseComObject(doc);
+            }
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            Document doc = null;
+            Style normalStyle = null;
+            Style heading1Style = null;
+            Style heading2Style = null;
+
+            try
+            {
+                doc = Globals.ThisAddIn.Application.ActiveDocument;
+
+                if (doc == null)
+                {
+                    System.Windows.Forms.MessageBox.Show("Нет активного документа.");
+                    return;
+                }
+                // Установка полей документа
+                doc.PageSetup.TopMargin = Globals.ThisAddIn.Application.CentimetersToPoints(2);
+                doc.PageSetup.BottomMargin = Globals.ThisAddIn.Application.CentimetersToPoints(2);
+                doc.PageSetup.LeftMargin = Globals.ThisAddIn.Application.CentimetersToPoints(3);
+                doc.PageSetup.RightMargin = Globals.ThisAddIn.Application.CentimetersToPoints((float)1.5);
+            }
+
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Произошла ошибка: " + ex.Message);
+            }
+            finally
+            {
+                if (normalStyle != null) Marshal.ReleaseComObject(normalStyle);
+                if (heading1Style != null) Marshal.ReleaseComObject(heading1Style);
+                if (heading2Style != null) Marshal.ReleaseComObject(heading2Style);
+                if (doc != null) Marshal.ReleaseComObject(doc);
+            }
+        }
+
+        private void button3_Click(object sender, RibbonControlEventArgs e)
+        {
+            Document doc = null;
+            Style normalStyle = null;
+            Style heading1Style = null;
+            Style heading2Style = null;
+
+            try
+            {
+                doc = Globals.ThisAddIn.Application.ActiveDocument;
+
+                if (doc == null)
+                {
+                    System.Windows.Forms.MessageBox.Show("Нет активного документа.");
+                    return;
+                }
+                // Установка полей документа
+                doc.PageSetup.TopMargin = Globals.ThisAddIn.Application.CentimetersToPoints(2);
+                doc.PageSetup.BottomMargin = Globals.ThisAddIn.Application.CentimetersToPoints(2);
+                doc.PageSetup.LeftMargin = Globals.ThisAddIn.Application.CentimetersToPoints(3);
+                doc.PageSetup.RightMargin = Globals.ThisAddIn.Application.CentimetersToPoints(1);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Произошла ошибка: " + ex.Message);
+            }
+            finally
+            {
+                if (normalStyle != null) Marshal.ReleaseComObject(normalStyle);
+                if (heading1Style != null) Marshal.ReleaseComObject(heading1Style);
+                if (heading2Style != null) Marshal.ReleaseComObject(heading2Style);
+                if (doc != null) Marshal.ReleaseComObject(doc);
+            }
+        }
+
+        private void button4_Click(object sender, RibbonControlEventArgs e)
+        {
+            Document doc = null;
+            Style normalStyle = null;
+            Style heading1Style = null;
+            Style heading2Style = null;
+
+            try
+            {
+                doc = Globals.ThisAddIn.Application.ActiveDocument;
+
+                if (doc == null)
+                {
+                    System.Windows.Forms.MessageBox.Show("Нет активного документа.");
+                    return;
+                }
+
+                Style pictureStyle = null;
+
+                // Проверка и настройка стиля "Картинка"
+                try
+                {
+                    pictureStyle = doc.Styles["Картинка"];
+                }
+                catch
+                {
+                    pictureStyle = doc.Styles.Add("Картинка", Word.WdStyleType.wdStyleTypeParagraph);
+                }
+
+                pictureStyle.ParagraphFormat = new Word.ParagraphFormat
+                {
+                    Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter,
+                    SpaceBefore = 28,
+                    SpaceAfter = 0,
+                    LineSpacingRule = Word.WdLineSpacing.wdLineSpace1pt5,
+                    FirstLineIndent = 0
+                };
+                pictureStyle.set_BaseStyle("");
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Произошла ошибка: " + ex.Message);
+            }
+            finally
+            {
+                if (normalStyle != null) Marshal.ReleaseComObject(normalStyle);
+                if (heading1Style != null) Marshal.ReleaseComObject(heading1Style);
+                if (heading2Style != null) Marshal.ReleaseComObject(heading2Style);
+                if (doc != null) Marshal.ReleaseComObject(doc);
+            }
+        }
+
+        private void button5_Click(object sender, RibbonControlEventArgs e)
+        {
+            Document doc = null;
+            Style normalStyle = null;
+            Style heading1Style = null;
+            Style heading2Style = null;
+
+            try
+            {
+                doc = Globals.ThisAddIn.Application.ActiveDocument;
+
+                if (doc == null)
+                {
+                    System.Windows.Forms.MessageBox.Show("Нет активного документа.");
+                    return;
+                }
+
+                Style pictureCaptionStyle = null;
+
+                try
+                {
+                    pictureCaptionStyle = doc.Styles["Подпись картинки"];
+                }
+                catch
+                {
+                    pictureCaptionStyle = doc.Styles.Add("Подпись картинки", Word.WdStyleType.wdStyleTypeParagraph);
+                }
+
+                pictureCaptionStyle.ParagraphFormat = new Word.ParagraphFormat
+                {
+                    Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter,
+                    SpaceBefore = 0,
+                    SpaceAfter = 28,
+                    FirstLineIndent = 0,
+                    LineSpacingRule = Word.WdLineSpacing.wdLineSpace1pt5
+                };
+
+                pictureCaptionStyle.set_BaseStyle("");
+            }
+
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Произошла ошибка: " + ex.Message);
+            }
+            finally
+            {
+                if (normalStyle != null) Marshal.ReleaseComObject(normalStyle);
+                if (heading1Style != null) Marshal.ReleaseComObject(heading1Style);
+                if (heading2Style != null) Marshal.ReleaseComObject(heading2Style);
+                if (doc != null) Marshal.ReleaseComObject(doc);
+            }
+        }
+
+        private void button6_Click(object sender, RibbonControlEventArgs e)
+        {
+            Document doc = null;
+            Style normalStyle = null;
+            Style heading1Style = null;
+            Style heading2Style = null;
+
+            try
+            {
+                doc = Globals.ThisAddIn.Application.ActiveDocument;
+
+                if (doc == null)
+                {
+                    System.Windows.Forms.MessageBox.Show("Нет активного документа.");
+                    return;
+                }
+
+                Style tableCaptionStyle = null;
+
+                try
+                {
+                    tableCaptionStyle = doc.Styles["Подпись таблицы"];
+                }
+                catch
+                {
+                    tableCaptionStyle = doc.Styles.Add("Подпись таблицы", Word.WdStyleType.wdStyleTypeParagraph);
+                }
+
+                tableCaptionStyle.ParagraphFormat = new Word.ParagraphFormat
+                {
+                    Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft,
+                    SpaceBefore = 28,
+                    SpaceAfter = 0,
+                    FirstLineIndent = Globals.ThisAddIn.Application.CentimetersToPoints((float)1.25),
+                    LineSpacingRule = Word.WdLineSpacing.wdLineSpace1pt5
+                };
+                tableCaptionStyle.set_BaseStyle("");
+            }
+
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Произошла ошибка: " + ex.Message);
+            }
+            finally
+            {
+                if (normalStyle != null) Marshal.ReleaseComObject(normalStyle);
+                if (heading1Style != null) Marshal.ReleaseComObject(heading1Style);
+                if (heading2Style != null) Marshal.ReleaseComObject(heading2Style);
+                if (doc != null) Marshal.ReleaseComObject(doc);
+            }
+        }
+    }
+}
